d82a95aec691c5569a8c213e692720c1de2ce24b
jd <jean.daniel.michaud@gmail.com>
2023-03-19 19:53:41 +0100

    Add license

--- generate-dicom-tags.sh
+++ generate-dicom-tags.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# Copyright (c) 2023 Jean-Daniel Michaud
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 if [ $# -ne 1 ]
 then
   echo "error: expected one parameter"
@@ -7,6 +27,26 @@ then
   exit 1
 fi
 
+echo '// Copyright (c) 2023 Jean-Daniel Michaud'
+echo '//'
+echo '// Permission is hereby granted, free of charge, to any person obtaining a copy'
+echo '// of this software and associated documentation files (the "Software"), to deal'
+echo '// in the Software without restriction, including without limitation the rights'
+echo '// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell'
+echo '// copies of the Software, and to permit persons to whom the Software is'
+echo '// furnished to do so, subject to the following conditions:'
+echo '//'
+echo '// The above copyright notice and this permission notice shall be included in all'
+echo '// copies or substantial portions of the Software.'
+echo '//'
+echo '// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR'
+echo '// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,'
+echo '// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE'
+echo '// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER'
+echo '// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,'
+echo '// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE'
+echo '// SOFTWARE.'
+echo ''
 echo '#![allow(dead_code)]'
 echo '#![allow(unused_variables)]'
 echo '#![allow(non_upper_case_globals)]'
